1f9d059afb3d61a1c7ded548cdfec76b67d1514e
Konstantin Maurin <artsjesus7@gmail.com>
2024-10-29 21:20:30 +0300

    add mailing urls

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("main.urls", namespace="main")),
+    path("mailing/", include("mailing.urls", namespace="mailing"))
 
 ]
