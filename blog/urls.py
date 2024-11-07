e440ebde58c9e4fe9b83b32deaacb33bf80c3573
Konstantin Maurin <artsjesus7@gmail.com>
2024-11-08 01:05:13 +0300

    fix name

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("blog/create/", BlogCreateView.as_view(), name="blog_create"),
     path("blog/<int:pk>/update/", BlogUpdateView.as_view(), name="blog_update"),
     path("blog/<int:pk>/delete/", BlogDeleteView.as_view(), name="blog_delete"),
-    path("blog/<int:pk>/", BlogDetailView.as_view(), name="Blog_detail")
+    path("blog/<int:pk>/", BlogDetailView.as_view(), name="blog_detail")
 ]
