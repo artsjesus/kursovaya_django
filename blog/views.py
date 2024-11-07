488db05830e6cae6d545dca18a5d1ac9907de12c
Konstantin Maurin <artsjesus7@gmail.com>
2024-11-08 01:05:38 +0300

    create

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from blog.models import Blog
+from django.views.generic import ListView, DeleteView, UpdateView, CreateView, DetailView
+from django.urls import reverse_lazy
+from blog.forms import BlogForm
 
-# Create your views here.
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = "blog/blog_list.html"
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = "blog/blog_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    form_class = BlogForm
+    success_url = reverse_lazy("blog:blog_list")
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    form_class = BlogForm
+    success_url = reverse_lazy("blog:blog_list")
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy("blog:blog_list")
