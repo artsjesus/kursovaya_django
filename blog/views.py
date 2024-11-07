0c3003b71d243f62c0f82e9c2d7ca981ae298b77
Konstantin Maurin <artsjesus7@gmail.com>
2024-11-08 01:36:22 +0300

    fix

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from blog.models import Blog
 from django.views.generic import ListView, DeleteView, UpdateView, CreateView, DetailView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from blog.forms import BlogForm
+from pytils.translit import slugify
 
 
 class BlogListView(ListView):
@@ -11,7 +12,6 @@ class BlogListView(ListView):
 
 class BlogDetailView(DetailView):
     model = Blog
-    template_name = "blog/blog_detail.html"
 
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
@@ -25,11 +25,27 @@ class BlogCreateView(CreateView):
     form_class = BlogForm
     success_url = reverse_lazy("blog:blog_list")
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
     form_class = BlogForm
-    success_url = reverse_lazy("blog:blog_list")
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
